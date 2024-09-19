813e4c062ca56f24d4a1af0be0f7ee657b3a37df
Phil Krylov <phil@krylov.eu>
2024-09-19 18:00:20 +0200

    test

--- bson/_cbsonmodule.c
+++ bson/_cbsonmodule.c
@@ -3029,7 +3029,6 @@ static PyModuleDef_Slot _cbson_slots[] = {
     {Py_mod_multiple_interpreters, Py_MOD_MULTIPLE_INTERPRETERS_SUPPORTED},
 #endif
 #ifdef Py_GIL_DISABLED
-    beliberda)
     {Py_mod_gil, Py_MOD_GIL_NOT_USED},
 #else
     erunda)
